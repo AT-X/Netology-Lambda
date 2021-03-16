376492cc961ec2b71db516ca39d66558c98cf509
AT-X <tumanovvan@gmail.com>
2021-03-16 17:37:16 +0300

    1v

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public Main() {
     }
+    public static void main (String[]args){
+
+        Calculator calc = (Calculator) Calculator.instance.get();
+        int a = (Integer) calc.plus.apply(1, 2);
+        int b = (Integer) calc.minus.apply(7, 5);
+        int c = (Integer) calc.divide.apply(a, b);
+        calc.println.accept(c);
+        calc.println.accept(calc.abs.apply(0));
+        calc.println.accept(calc.abs.apply(-7));
+        calc.println.accept(calc.abs.apply(7));
+        calc.println.accept(calc.abs.apply(-2147483648));
+        calc.println.accept(calc.abs.apply(2147483647));
+    }
+
 }
+
