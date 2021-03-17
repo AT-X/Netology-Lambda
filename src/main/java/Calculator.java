5988a69000bfe261c4fe43556211eefc6dc26949
AT-X <tumanovvan@gmail.com>
2021-03-17 11:50:33 +0300

    2v

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,27 +8,20 @@ import java.util.function.UnaryOperator;
 
 public class Calculator {
     static Supplier<Calculator> instance = Calculator::new;
-    BinaryOperator<Integer> plus = (x, y) -> {
-        return x + y;
-    };
-    BinaryOperator<Integer> minus = (x, y) -> {
-        return x - y;
-    };
-    BinaryOperator<Integer> multiply = (x, y) -> {
-        return x * y;
-    };
-    BinaryOperator<Integer> divide = (x, y) -> {
-        return x / y;
-    };
-    UnaryOperator<Integer> pow = (x) -> {
-        return x * x;
-    };
-    UnaryOperator<Integer> abs = (x) -> {
-        return x > 0 ? x : x * -1;
-    };
-    Predicate<Integer> isPositive = (x) -> {
-        return x > 0;
-    };
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+    BinaryOperator<Integer> divide = (x, y) -> x / y;
+
+    UnaryOperator<Integer> pow = (x) -> x * x;
+
+    UnaryOperator<Integer> abs = (x) -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = (x) -> x > 0;
+
     Consumer<Integer> println;
 
     public Calculator() {
